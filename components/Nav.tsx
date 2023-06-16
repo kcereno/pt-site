9fb0af22fae76a2a1736eda70cde22d4820ada5a
Karl Cereno <kagc.dev@gmail.com>
2023-06-16 13:47:55 -0400

    remove bootstrap

---
+++ components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <span className="mb-0 navbar-brand h1">Navbar</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
