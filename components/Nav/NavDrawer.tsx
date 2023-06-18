a84f28785ef6a51ef346066f33e20c355fe9c0a6
Karl Cereno <kagc.dev@gmail.com>
2023-06-18 16:24:25 -0400

    Finish mobile side drawer

--- components/Nav/NavDrawer.tsx
+++ components/Nav/NavDrawer.tsx
@@ -1,18 +1,27 @@
+'use client';
 import HamburgerMenuIcon from '@/ui/icons/HamburgerMenuIcon';
+import Link from 'next/link';
 import React from 'react';
 
 const NavDrawer = () => {
+  const navLink = 'hover:bg-gray-200 hover:text-gray-900 rounded-md';
+
+  const handleCloseButtonClick = () => {
+    const drawer = document.getElementById('mobile-nav-drawer');
+    drawer?.click();
+  };
+
   return (
     <div className="drawer drawer-end">
       <input
-        id="my-drawer"
+        id="mobile-nav-drawer"
         type="checkbox"
         className="drawer-toggle"
       />
       <div className="drawer-content">
         {/* Page content here */}
         <label
-          htmlFor="my-drawer"
+          htmlFor="mobile-nav-drawer"
           className="btn btn-ghost drawer-button"
         >
           <HamburgerMenuIcon />
@@ -25,9 +34,60 @@ const NavDrawer = () => {
         ></label>
         <div className="flex h-full p-4 menu w-80 bg-base-200 text-base-content">
           {/* Sidebar content here */}
-          <ul className="my-auto">
+          <div className="absolute right-1">
+            <button
+              className="btn btn-block"
+              onClick={handleCloseButtonClick}
+            >
+              X
+            </button>
+          </div>
+          <ul className="w-auto my-auto menu menu-lg bg-base-200 rounded-box">
             <li>
-              <a>About</a>
+              <Link
+                href="/"
+                className={navLink}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className={navLink}
+              >
+                Services
+              </Link>
+            </li>
+
+            <li>
+              <a>Resources</a>
+              <ul className="p-2">
+                <li>
+                  <Link
+                    href="/"
+                    className={navLink}
+                  >
+                    Insurance
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/"
+                    className={navLink}
+                  >
+                    New Patient Forms
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className={navLink}
+              >
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
