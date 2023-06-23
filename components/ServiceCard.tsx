55269c711a9973e5ce91202fe5a9afa9ce6c0c57
Karl Cereno <kagc.dev@gmail.com>
2023-06-23 18:36:43 -0400

    Change Image tag in Services

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface Props {
@@ -9,10 +10,12 @@ interface Props {
 const ServiceCard = ({ title, description, imageSrc }: Props) => {
   return (
     <div className="flex-1 mt-10 md:mt-0">
-      <img
+      <Image
         className="self-center object-cover w-full rounded-lg shadow-lg h-60 lg:h-40"
         src={imageSrc}
-        alt=""
+        alt={title}
+        width={600}
+        height={600}
       />
       <div className="mt-5">
         <p className="text-xl bold-heading">{title}</p>
