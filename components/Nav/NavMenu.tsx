4e1d9d663d7d9b4e792b872b9e7ec1513fe9a05a
Karl Cereno <kagc.dev@gmail.com>
2023-06-23 11:28:34 -0400

    Add Link hover classes

--- components/Nav/NavMenu.tsx
+++ components/Nav/NavMenu.tsx
@@ -1,30 +1,57 @@
 import Link from 'next/link';
 
 const NavMenu = () => {
+  const linkClasses = ' hover:text-secondary';
+
   return (
     <ul className="px-1 menu lg:menu-lg menu-horizontal">
       <li>
-        <Link href="/">About Us</Link>
+        <Link
+          className={linkClasses}
+          href="/about"
+        >
+          About Us
+        </Link>
       </li>
 
       <li>
-        <Link href="/">Services</Link>
+        <Link
+          className={linkClasses}
+          href="/"
+        >
+          Services
+        </Link>
       </li>
       <li tabIndex={0}>
         <details>
           <summary>Resources</summary>
           <ul className="p-2">
             <li>
-              <Link href="/">Insurance</Link>
+              <Link
+                className={linkClasses}
+                href="/"
+              >
+                Insurance
+              </Link>
             </li>
             <li>
-              <Link href="/">New Patient Forms</Link>
+              <Link
+                className={linkClasses}
+                href="/"
+              >
+                New Patient Forms
+              </Link>
             </li>
           </ul>
         </details>
       </li>
       <li>
-        <Link href="/">Contact Us</Link>
+        <Link
+          className={linkClasses}
+          href="/"
+        >
+          Contact Us
+        </Link>
       </li>
     </ul>
   );
